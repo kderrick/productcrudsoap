6337cfba8878cd80c37bbaebc65473d7d4d87708
Kyle <kderrick1988@gmail.com>
2019-02-18 09:27:37 -0800

    initial commit

---
+++ src/main/java/com/kyle/productcrud/soap/ProductcrudsoapApplication.java
@@ -0,0 +1,13 @@
+package com.kyle.productcrud.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductcrudsoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductcrudsoapApplication.class, args);
+	}
+
+}
