9710c38d006e7f4bf4a26a1f8c614ee3b30bde77
Kyle <kderrick1988@gmail.com>
2019-02-27 16:03:59 -0800

    implement methods within ProductWsImpl

--- src/main/java/com/kyle/productcrud/soap/ProductWsImpl.java
+++ src/main/java/com/kyle/productcrud/soap/ProductWsImpl.java
@@ -2,32 +2,34 @@ package com.kyle.productcrud.soap;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.kyle.productcrud.soap.entities.Product;
+import com.kyle.productcrud.soap.repos.ProductRepository;
 
 public class ProductWsImpl implements ProductWs {
+	
+	@Autowired
+	private ProductRepository productRepo;
 
 	@Override
 	public List<Product> getProducts() {
-		// TODO Auto-generated method stub
-		return null;
+		return productRepo.findAll();
 	}
 
 	@Override
 	public Product getProduct(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		return productRepo.findById(id).get();
 	}
 
 	@Override
 	public Product createProduct(Product product) {
-		// TODO Auto-generated method stub
-		return null;
+		return productRepo.save(product);
 	}
 
 	@Override
 	public Product updateProduct(Product product) {
-		// TODO Auto-generated method stub
-		return null;
+		return productRepo.save(product);
 	}
 
 }
