2e21c973ab557696b1980ddef666fb74055a9eaf
Kyle <kderrick1988@gmail.com>
2019-02-28 16:43:37 -0800

    add endpoints with annotations in ProductWs interface

--- src/main/java/com/kyle/productcrud/soap/ProductWs.java
+++ src/main/java/com/kyle/productcrud/soap/ProductWs.java
@@ -2,16 +2,23 @@ package com.kyle.productcrud.soap;
 
 import java.util.List;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
 import com.kyle.productcrud.soap.entities.Product;
 
+@WebService
 public interface ProductWs {
 
+	@WebMethod
 	List<Product> getProducts();
 	
-	
+	@WebMethod
 	Product getProduct(int id);
 	
+	@WebMethod
 	Product createProduct(Product product);
 	
+	@WebMethod
 	Product updateProduct(Product product);
 }
